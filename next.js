74eafeb55e0da2dae804b5065c4ebddb6af95257
Airej Tashfeen <airejtashfeen620@gmail.com>
2023-11-08 17:29:22 +0500

    Login Page added

---
+++ next.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const sql = require('mssql');
+const app = express();
+const port = 3000;
+
+const config = {
+    user: 'sa',
+    password: 'Imbatman',
+    server: 'localhost',
+    database: 'KitchenWardrobes',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true
+    }
+};
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    sql.connect(config, (err) => {
+        if (err) {
+            console.log('Failed to connect to SQL Server.', err);
+            res.status(500).send('Failed to connect to the database');
+        } else {
+            const request = new sql.Request();
+            request.query(`SELECT * FROM dbo.Users WHERE Username = '${username}' AND Password = '${password}'`, (err, result) => {
+                if (err) {
+                    console.log('Failed to execute SQL query.');
+                    res.status(500).send('Failed to execute SQL query');
+                } else {
+                    if (result.recordset.length > 0) {
+                        // User is authenticated
+                        res.send('Welcome! Login Successful');
+                    } else {
+                        // Invalid credentials
+                        res.send('Invalid username or password');
+                    }
+                }
+            });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
